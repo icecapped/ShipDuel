dc53fa42a161e61c9b13d01be2bf5f7c34cc2627
liujeffery <37119420+liujeffery@users.noreply.github.com>
2019-01-15 22:25:18 -0500

    Update GUI.java

--- ShipDuel/src/main/GUI.java
+++ ShipDuel/src/main/GUI.java
@@ -7,123 +7,123 @@ import javax.swing.*;
 public class GUI {
 	public static void main(String[] args) {
 		Frame frame = new Frame();
-		frame.setSize(720, 900);
+		frame.setSize(720, 945);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		frame.setResizable(false);
 	}
 }
 
 class Frame extends JFrame implements MouseListener {
-	boolean isMainMenu;
-	//fixed distances to move the "back" button image into the correct position
-	int backxfix = 8;
-	int backyfix = 31;
-	//fixed distances to move the "rules" and "play" button into the corret position
-	//xfix not needed
-	int xfix = 95;
-	int yfix = 15;
+	static final Image BG_IMAGE = new ImageIcon("src/assets/CosmicSky.png").getImage();
+	static final Image TITLE = new ImageIcon("src/assets/title.png").getImage();
+	static final Image RULES = new ImageIcon("src/assets/rulesImg.png").getImage();
+	static final ImageIcon BLOCK = new ImageIcon("src/assets/block.png");
 	
+	boolean isMainMenu = false;
 	Container c;
+	
 	CardLayout cards;
-	JPanel one;
-	JPanel two;
+	JPanel mainMenu;
+	JPanel rules;
 	JPanel mainMenuFormat;
 	JPanel backFormat;
+	
 	button play;
 	button goToRules;
 	button goToMainMenu;
+	
 	MFrame frame;
 
 	public Frame() {
 		super("menu");
-		isMainMenu = false;
-		
-		frame = new MFrame("game");
-		
-		c = getContentPane();
-		cards = new CardLayout();
-		c.setLayout(cards);
 
-		//main menu panel
-		one = new JPanel();
-		one.setLayout(new BorderLayout());
+		c = getContentPane();
 		
-		//rules panel
-		two = new JPanel();
-		two.setLayout(new BorderLayout());
-		
-		//JPanel used to size the main menu properly
+		cards = new CardLayout();
+		mainMenu = new JPanel();
+		rules = new JPanel();
 		mainMenuFormat = new JPanel();
-		mainMenuFormat.setLayout(new GridLayout(5, 4));
-		
 		backFormat = new JPanel();
-		backFormat.setLayout(new GridLayout(1, 6));
 		
 		goToMainMenu = new button(new ImageIcon("src/assets/back.png"), "mainMenu");
-		goToMainMenu.addMouseListener(this);
-		
 		goToRules = new button(new ImageIcon("src/assets/rules.png"), "rules");
-		goToRules.addMouseListener(this);
-		
 		play = new button (new ImageIcon("src/assets/play.png"), "");
-		play.addMouseListener(this);
 	
-		one.add(new JLabel(new ImageIcon("src/assets/block.png")), BorderLayout.NORTH);
-		one.add(new JLabel(new ImageIcon("src/assets/block.png")), BorderLayout.EAST);
-		one.add(new JLabel(new ImageIcon("src/assets/block.png")), BorderLayout.SOUTH);
-		one.add(new JLabel(new ImageIcon("src/assets/block.png")), BorderLayout.WEST);
-
-		one.add(mainMenuFormat, BorderLayout.CENTER);
+		goToMainMenu.addMouseListener(this);
+		goToRules.addMouseListener(this);
+		play.addMouseListener(this);
 		
+		setLayouts();
+		
+		populateMainMenuFormat();
+		mainMenu.add(mainMenuFormat, BorderLayout.CENTER);
+		
+		populateBackFormat();
+		rules.add(backFormat, BorderLayout.NORTH);
+		
+		c.add(mainMenu, "mainMenu");
+		c.add(rules, "rules");
+		
+	}
+	public void setLayouts() {
+		c.setLayout(cards);
+		
+		mainMenu.setLayout(new BorderLayout());
+		rules.setLayout(new BorderLayout());
+		mainMenuFormat.setLayout(new GridLayout(5, 4));
+		backFormat.setLayout(new GridLayout(1, 6));
+	}
+	
+	public void populateMainMenuFormat() {
 		for (int i = 0; i < 12; i = i +1)
 			mainMenuFormat.add(new JLabel());
 		mainMenuFormat.add(goToRules);
+		
 		for (int i = 0; i < 2; i = i + 1)
 			mainMenuFormat.add(new JLabel());
 		mainMenuFormat.add(play);
+		
 		for (int i = 0; i < 4; i = i + 1)
 			mainMenuFormat.add(new JLabel());
-
-		two.add(backFormat, BorderLayout.NORTH);
-		two.add(new JLabel(new ImageIcon("src/assets/block.png")), BorderLayout.CENTER);
-		two.add(new JLabel(new ImageIcon("src/assets/block.png")), BorderLayout.EAST);
-		two.add(new JLabel(new ImageIcon("src/assets/block.png")), BorderLayout.SOUTH);
-		two.add(new JLabel(new ImageIcon("src/assets/block.png")), BorderLayout.WEST);
-		
+	}
+	
+	public void populateBackFormat() {
 		backFormat.add(goToMainMenu);
 		for (int i = 0; i < 5; i = i + 1)
-			backFormat.add(new JLabel(new ImageIcon("src/assets/bigBlock.png")));
-		
-		c.add(one, "mainMenu");
-		c.add(two, "rules");
-		
+			backFormat.add(new JLabel(BLOCK));		
 	}
+	
 	public void paint (Graphics g) {
 		super.paint(g);
-		g.drawImage(new ImageIcon("src/assets/CosmicSky.png").getImage(), 0, 0, null);
-		
+		g.drawImage(BG_IMAGE, 0, 0, null);
 		if (!isMainMenu) {
-			g.drawImage(goToRules.image.getImage() , goToRules.getX() + xfix, goToRules.getY() + yfix, null);
-			g.drawImage(play.image.getImage(), play.getX() + xfix, play.getY() + yfix, null);
-			
+			g.drawImage(TITLE, 32, 100, null);
+			g.drawImage(goToRules.image, 3, 580, null);
+			g.drawImage(play.image, 538, 580, null);
 		}
 		else{
-			g.drawImage(goToMainMenu.image.getImage(), goToMainMenu.getX() + backxfix, goToMainMenu.getY() + backyfix, null);
+			g.drawImage(goToMainMenu.image, 3, 28, null);
+			g.drawImage(RULES, 150, 150, null);
 		}
 		isMainMenu = !isMainMenu;
 	}
 	public void mouseClicked(MouseEvent e) {
 		
-		if (e.getComponent() == goToMainMenu)
+		if (e.getComponent() == goToMainMenu) {
 			cards.show(c, goToMainMenu.change);
-		else if (e.getComponent() == goToRules)
+			repaint();
+		}
+		else if (e.getComponent() == goToRules) {
 			cards.show(c, goToRules.change);
+			repaint();
+		}
 		else {
-			cards.show(c, "play");
-			frame.setSize(825, 990);
+			frame = new MFrame("game");
+			frame.setSize(870, 970);
+			frame.setResizable(false);
 			frame.setVisible(true);
 		}
-		repaint();
 	}
 
 	public void mouseEntered(MouseEvent e) {
@@ -142,18 +142,13 @@ class Frame extends JFrame implements MouseListener {
 }
 
 class button extends JPanel {
-	ImageIcon image;
+	Image image;
 	String change;
 
 	public button(ImageIcon image, String change) {
 		super();
 
 		this.change = change;
-		this.image = image;
+		this.image = image.getImage();
 	}
 }
-
-
-
-
-
